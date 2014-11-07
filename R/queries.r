06d9deb7d23dded492158f3f0f45675980a95611
glamp <lamp.greg@gmail.com>
2014-11-07 17:03:59 -0500

    more packages

--- R/queries.r
+++ R/queries.r
@@ -1,7 +1,6 @@
 library(rjson)
 
-
-queries.pg <- rjson::fromJSON(paste(readLines("./postgres.json"), collapse=""))
-queries.redshift <- rjson::fromJSON(paste(readLines("./redshift.json"), collapse=""))
-queries.sqlite <- rjson::fromJSON(paste(readLines("./sqlite.json"), collapse=""))
-queries.mysql <- rjson::fromJSON(paste(readLines("./mysql.json"), collapse=""))
+queries.pg <- rjson::fromJSON(paste(readLines(system.file("include", "postgres.json")), collapse=""))
+queries.redshift <- rjson::fromJSON(paste(readLines(system.file("include", "redshift.json")), collapse=""))
+queries.sqlite <- rjson::fromJSON(paste(readLines(system.file("include", "sqlite.json")), collapse=""))
+queries.mysql <- rjson::fromJSON(paste(readLines(system.file("include", "mysql.json")), collapse=""))
